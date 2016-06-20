2a1d57be98c4828db5def804a8940a77b4b2190d
Will Gu <willgu29@gmail.com>
2016-06-20 11:46:08 -0700

    Heroku push one

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/record', function(req, res, next) {
+  res.render('record');
+});
+
+
 module.exports = router;
