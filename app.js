34e8a489768bd60e6e53068a3e1e4d245d475dc5
Will Gu <willgu29@gmail.com>
2016-06-27 11:59:19 -0700

    Deleted unneeded code

--- app.js
+++ app.js
@@ -67,41 +67,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-/////PASSPORT Session///////////////
-// var LocalStrategy = require('passport-local').Strategy;
-// var User = require('./models/User.js');
-//
-// passport.use(new LocalStrategy({
-// 		usernameField: "email",
-// 		passwordField: "password"
-// 	},
-// 	function (email, password, done) {
-// 		User.findOne({email: email}, function (err, user) {
-// 			if (err) {return done(err);}
-// 			if (!user) {
-// 				console.log("Incorrect email");
-//        			return done(null, false, { message: 'Incorrect email.' });
-//       		}
-//       		if (!(user.password == password)) {
-//       			console.log("Incorrect password");
-//         		return done(null, false, { message: 'Incorrect password.' });
-//       		}
-//       		return done(null, user);
-// 		});
-// 	}
-// ));
-//
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
-//
-// passport.deserializeUser(function(id, done) {
-//   User.findById(id, function (err, user) {
-//     done(err, user);
-//   });
-// });
-
 /// MONGOOOSE Database Linking ****
 var mongoose = require('mongoose');
 
