0b4751833102c97a21dec81b8625b2976638ea36
Will Gu <willgu29@gmail.com>
2016-07-06 11:22:48 -0700

    Testing upload to speechmatics

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,6 +1,10 @@
 function upload(audioFile) {
+
+  console.log(audioFile.files[0]);
+  alert("hello");
+
   var json = {
-    "data_file"    : audioFile,
+    "data_file"    : audioFile.files[0],
     "model"        : "en-US,",
     "notification" : "callback",
     "callback"     : "localhost:3000/speechmatics/process"
@@ -9,11 +13,12 @@ function upload(audioFile) {
   $.ajax({
       url: "https://api.speechmatics.com/v1.0/user/3621/jobs/?auth_token=MDBhM2Q3YWEtODI3OS00MDA1LWFjNzAtMjE0OGJjYWEzNjQ0",
       type: "POST",
-      data: formdata,
+      data: json,
       processData: false,
       contentType: false,
       success: function (res) {
         document.getElementById("response").innerHTML = res;
       }
   });
+
 }
