a3b47e27244af768f32fb41b5969f4a13fa2ebe5
Adam Garcia <adamgarcia1994@ucla.edu>
2016-07-17 17:41:54 -0700

    Finished AWS Uploading process.  Now Just need to store get URL wherever desired.

--- routes/amazon.js
+++ routes/amazon.js
@@ -1,6 +1,7 @@
 // Amazon S3 Upload Route
 
 //*******************Import Dependencies***************
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 var aws = require('aws-sdk');
@@ -11,20 +12,67 @@ console.log('s3 bucket is: ' + S3_BUCKET);
 
 //***********************Routes************************
 router.get('/sign_s3', function(req, res) {
-  //Route to get a signed url
-  console.log('hiya1');
-  console.log("query: ",req.query.file_name, req.query.file_type);
   
-  //**********AWS Config***********
+    // //**********AWS Config***********
   aws.config.update({
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey : AWS_PRIVATE_KEY
   });
-
   aws.config.region = 'us-west-2';
-  
-  //**********AWS S3 Init**********
+  //console.log('AKI : '+ AWS_ACCESS_KEY + 'PKI : ' + AWS_PRIVATE_KEY);
+
   var s3 = new aws.S3();
+  //console.log('Start Sign s3');
+  const fileName = req.query['file_name'];
+  const fileType = req.query['file_type'];
+
+  //console.log('about to set S3 Params');
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  console.log('Get Signed Url');
+
+  s3.getSignedUrl('putObject', s3Params, function(err, data) {
+    if(err) {
+      console.log(err);
+      return res.end();
+    }
+
+    const returnData = {
+      signedRequest : data,
+      url: 'https://s3-us-west-2.amazonaws.com/' + S3_BUCKET + '/' + fileName
+    }
+    console.log('Signed Request is: ' + returnData.signedRequest);
+    console.log('url is: ' + returnData.url);
+
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
+
+
+
+
+  // //Route to get a signed url
+  // console.log('hiya1');
+  // console.log("query: ",req.query.file_name, req.query.file_type);
+  
+  // //**********AWS Config***********
+  // aws.config.update({
+  //   accessKeyId: AWS_ACCESS_KEY,
+  //   secretAccessKey : AWS_PRIVATE_KEY
+  // });
+
+  // aws.config.region = 'us-west-2';
+  
+  // //**********AWS S3 Init**********
+  // var s3 = new aws.S3();
 
   //**************Presigned URL for GET Operation************
   // var params = {Bucket: S3_BUCKET, Key: 'test1'};
@@ -51,53 +99,53 @@ router.get('/sign_s3', function(req, res) {
 
   //***********Establish AWS S3 GET request*************************
   //var return_data;
-  var getURL;
-  var putURL;
-  var s3Params_GET = {
-  Bucket: S3_BUCKET,
-  Key: 'key1',
-  //ContentType: req.query.file_type,
-  //ACL: 'public-read'
-  };
+  // var getURL;
+  // var putURL;
+  // var s3Params_GET = {
+  // Bucket: S3_BUCKET,
+  // Key: 'key1',
+  // //ContentType: req.query.file_type,
+  // //ACL: 'public-read'
+  // };
 
-  s3.getSignedUrl('getObject', s3Params_GET, function(err, url) {
-    if(err) {
-      console.log('error: ',err);
-      throw(err);
-    } else {
-      console.log('GET OBJECT URL');
-      getURL = JSON.stringify(url);
-    }
-  });
+  // s3.getSignedUrl('getObject', s3Params_GET, function(err, url) {
+  //   if(err) {
+  //     console.log('error: ',err);
+  //     throw(err);
+  //   } else {
+  //     console.log('GET OBJECT URL');
+  //     getURL = JSON.stringify(url);
+  //   }
+  // });
 
-  //console.log(getURL);
+  // //console.log(getURL);
 
-  //***********Establish AWS S3 PUT request*************************
-    var s3Params_PUT = {
-    ACL: 'public-read',
-    Bucket: S3_BUCKET,
-    Key: 'key1',
-    ContentType: req.query.file_type,
-  };
+  // //***********Establish AWS S3 PUT request*************************
+  //   var s3Params_PUT = {
+  //   ACL: 'public-read',
+  //   Bucket: S3_BUCKET,
+  //   Key: 'key1',
+  //   ContentType: req.query.file_type,
+  // };
 
-  s3.getSignedUrl('putObject', s3Params_PUT, function(err, url) {
-    if(err) {
-      console.log('error: ',err);
-      throw(err);
-    } else {
-      console.log('PUT OBJECT');
-      putURL = url;
+  // s3.getSignedUrl('putObject', s3Params_PUT, function(err, url) {
+  //   if(err) {
+  //     console.log('error: ',err);
+  //     throw(err);
+  //   } else {
+  //     console.log('PUT OBJECT');
+  //     putURL = url;
     
-      var return_data = {
-        URL_get : getURL,
-        URL_put : putURL
-      };
-      console.log('return data is: ');
-      console.log(JSON.stringify(return_data));
-      res.write(JSON.stringify(return_data));
-      res.end();
-    }
-  });
+  //     var return_data = {
+  //       URL_get : getURL,
+  //       URL_put : putURL
+  //     };
+  //     console.log('return data is: ');
+  //     console.log(JSON.stringify(return_data));
+  //     res.write(JSON.stringify(return_data));
+  //     res.end();
+  //   }
+  // });
 
   //console.log(putURL);
 
@@ -135,6 +183,6 @@ router.get('/sign_s3', function(req, res) {
   //   }
   // });
   // console.log('url complete is: ');
-});
+// });
 
 module.exports = router;
